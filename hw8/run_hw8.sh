2ad71a7ce2e65d9c39d1d295ae800ba0f3503b52
Gayatri Malladi <gayatrimalladi@Gayatris-MacBook-Pro.local>
2025-06-14 02:48:24 -0700

    Added hw8 folder with relevant files

---
+++ hw8/run_hw8.sh
@@ -0,0 +1,5 @@
+#To acticate the conda environment and run the script, use the following commands:
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /mnt/dropbox/24-25/574/env
+# To run the Running the Translation Model with full parameters , use the following command:
+python run.py
